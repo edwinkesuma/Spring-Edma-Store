293507ecf104e74505f6d08eb6ffd8d384e09448
Edwin Kesuma <edwin.kesuma.EK21@gmail.com>
2026-05-22 17:24:39 +0700

    first commit

---
+++ src/main/java/com/edwinkesuma/springedmastore/SpringEdmaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.edwinkesuma.springedmastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEdmaStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEdmaStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edwinkesuma/springedmastore/SpringEdmaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwinkesuma.springedmastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEdmaStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
