aa3da6e7faf2bc90c362ec141a82090cd1cebbaf
Edwin Kesuma <edwin.kesuma.EK21@gmail.com>
2026-05-24 20:30:03 +0700

    fix: change JpaRepository Id from Long to UUID at UserRepository

--- src/main/java/com/edwinkesuma/springedmastore/features/user/domain/repository/UserRepository.java
+++ src/main/java/com/edwinkesuma/springedmastore/features/user/domain/repository/UserRepository.java
@@ -7,8 +7,9 @@ import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findUserByEmail(String email);
 
     boolean existsByEmail(@NotBlank @Email @Size(max = 150) String email);
